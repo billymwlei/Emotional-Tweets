8af2866c09837ac5516123f7d98901e4fa9d24bc
Billy <billy.lei@me.com>
2014-02-23 21:02:10 +1000

    Initial Commit

---
+++ Emotional TweetsTests/Emotional_TweetsTests.m
@@ -0,0 +1,34 @@
+//
+//  Emotional_TweetsTests.m
+//  Emotional TweetsTests
+//
+//  Created by Billy on 23/02/2014.
+//  Copyright (c) 2014 Billy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Emotional_TweetsTests : XCTestCase
+
+@end
+
+@implementation Emotional_TweetsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
